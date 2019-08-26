ed11f5279643c12aa810bfc34db128a3b1720584
juniorkibirige <aanga26@gmail.com>
2019-08-26 22:24:40 +0300

    reading files

---
+++ styles/js/gethotels.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const fileContents = fs.readFileSync('./inc/rKData.json', 'utf8');
+
+try {
+    const data = JSON.parse(fileContents)
+    console.log(data[2][hotelName]
+} catch (error) {
+    console.log(error)
+}
