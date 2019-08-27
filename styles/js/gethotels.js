18e11dea3d377b634a9f91510aaa74924f8faac3
juniorkibirige <aanga26@gmail.com>
2019-08-27 09:07:36 +0300

    hj

--- styles/js/gethotels.js
+++ styles/js/gethotels.js
@@ -3,7 +3,7 @@ const fileContents = fs.readFileSync('./inc/rKData.json', 'utf8');
 
 try {
     const data = JSON.parse(fileContents)
-    console.log(data[2][hotelName]
+    console.log(data[2][hotelName])
 } catch (error) {
     console.log(error)
 }
